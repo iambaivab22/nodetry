c9e4e59bcbf71154b7afbc97c63cf317f8e18139
baivab <bidaribaivab7@gmail.com>
2024-06-23 15:28:54 +0545

    fix:jwt token added

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,84 @@
+// const User = require("../modals/userModal");
+
+// const express = require("express");
+// const router = express.Router();
+
+// const bcrypt = require("bcrypt");
+// const jwt = require("jsonwebtoken");
+
+// // Register
+// exports.registerUser = async (req, res) => {
+//   try {
+//     const { email, password } = req.body;
+
+//     if (!email || !password) {
+//       return res
+//         .status(400)
+//         .json({ message: "Please provide an email and password." });
+//     }
+
+//     const existingUser = await User.findOne({ email });
+
+//     if (existingUser) {
+//       return res.status(400).json({ message: "User already exists." });
+//     }
+
+//     // const hashedPassword = await bcrypt.hash(password, 10);
+//     console.log("userData", email, password);
+//     const user = new User({ email, password: password });
+//     await user.save();
+
+//     res.status(201).json({
+//       data: user,
+//       // userRoles: userRole,
+//       message: "User registered successfully.",
+//     });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ message: "Registration failed." });
+//   }
+// };
+
+// exports.loginUser = async (req, res) => {
+//   try {
+//     const { email, password } = req.body;
+//     const user = await User.findOne({ email });
+
+//     console.log(email);
+
+//     if (!user) {
+//       return res.status(401).json({ message: "Invalid email or password." });
+//     }
+
+//     const isPasswordValid = await bcrypt.compare(password, user.password);
+//     if (!isPasswordValid) {
+//       return res.status(401).json({ message: "Invalid email or password." });
+//     }
+//     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
+//     let userRoles = "";
+
+//     if (email === "meromail123@gmail.com" && password === "123456783") {
+//       console.log("admin");
+//       userRoles = "ADMIN";
+//     } else {
+//       userRoles = "USER";
+//       console.log("non admin");
+//     }
+
+//     console.log("userRoles", userRoles, "role");
+
+//     res.json({ token, user: user, userRoles: userRoles });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ message: "Login failed." });
+//   }
+// };
 const User = require("../modals/userModal");
 
 const express = require("express");
 const router = express.Router();
 
-// const bcrypt = require("bcrypt");
+const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 // Register
@@ -23,9 +98,9 @@ exports.registerUser = async (req, res) => {
       return res.status(400).json({ message: "User already exists." });
     }
 
-    // const hashedPassword = await bcrypt.hash(password, 10);
-    console.log("userData", email, password);
-    const user = new User({ email, password: password });
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = new User({ email, password: hashedPassword });
     await user.save();
 
     res.status(201).json({
@@ -44,17 +119,15 @@ exports.loginUser = async (req, res) => {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
 
-    console.log(email);
-
     if (!user) {
       return res.status(401).json({ message: "Invalid email or password." });
     }
 
-    // const isPasswordValid = await bcrypt.compare(password, user.password);
-    // if (!isPasswordValid) {
-    //   return res.status(401).json({ message: "Invalid email or password." });
-    // }
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid email or password." });
+    }
+    const token = jwt.sign({ userId: user._id }, "4Kq#W%L9gT!z7&$xP@jR");
     let userRoles = "";
 
     if (email === "meromail123@gmail.com" && password === "123456783") {
@@ -64,7 +137,6 @@ exports.loginUser = async (req, res) => {
       userRoles = "USER";
       console.log("non admin");
     }
-
     console.log("userRoles", userRoles, "role");
 
     res.json({ token, user: user, userRoles: userRoles });
