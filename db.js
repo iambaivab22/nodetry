952ed597fdf1c40d2e74be7627f8c7386cc48ca3
baivab <bidaribaivab7@gmail.com>
2024-06-23 11:25:12 +0545

    fix:mongo db url added

--- db.js
+++ db.js
@@ -1,9 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const dbHOST = process.env.DBHOST;
 
-mongoose.connect(dbHOST)
-    .then(() => {
-        console.log('MongoDB Connnected...')
-    }).catch((err) => {
-        console.log('Error while Mongo Conn..', err);
-    })
+mongoose
+  .connect(
+    "mongodb+srv://bidaribaivab7:(meropassword12)@cluster0.l1lbpsa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => {
+    console.log("MongoDB Connnected...");
+  })
+  .catch((err) => {
+    console.log("Error while Mongo Conn..", err);
+  });
