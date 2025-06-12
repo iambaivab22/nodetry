7dc43baffb2b7be3e860617f529f5b5a5f6db9e9
iambaivab22 <baivabbidari876@gmail.com>
2025-06-12 19:24:25 +0545

    fix/folder structure fixed for categories

--- handlers/multerCategory.handler.js
+++ handlers/multerCategory.handler.js
@@ -3,7 +3,7 @@ const multer = require("multer");
 
 const categoryStorage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "uploads/categories");
+    cb(null, "uploads/product");
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + "-" + file.originalname);
